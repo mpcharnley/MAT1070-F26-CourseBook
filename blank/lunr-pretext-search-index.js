3901ed7ce06160d6a38383c4b08879039c9e06a2
Matt Charnley <112104661+mpcharnley@users.noreply.github.com>
2026-09-02 03:15:06 +0000

    Latest build deployed.

--- blank/lunr-pretext-search-index.js
+++ blank/lunr-pretext-search-index.js
@@ -16,7 +16,7 @@ var ptx_lunr_docs = [
   "type": "Section",
   "number": "",
   "title": "Week 1",
-  "body": " Week 1   This is an outline of the topics we covered in the first week of class.     Monday 8\/31     Introduction to the class    Introduction to graphing solutions of equations.       Introduction to the Course  Welcome to College Algebra!  What are the goals of this course?       Introduction to the People  Who am I?  Who are all of you? Take about 3 minutes to introduce yourself to the people around you. These are going to be your companions on this semester-long journey that we have together. When we come back together, I'll want everyone to introduce themselves with their name and intended major\/career path.      Syllabus and Canvas Overview  First off, the syllabus. The entire syllabus is available on our Canvas site.     Basic info    Qualifying    eBook (more on that within Canvas later)    Attendance    Student Hours    Homework    Quizzes and Tests    Grade Policy and Final Exam Requirements    AI and Calculators     For the Canvas site:   Home page    Modules    Agenda page with notes    Pearson homework and eBook         Graphing  We're going to start this course by talking about graphing. There are many different types of graph, but the one we want to talk about in this class is the kind the allows us to compare two quantities. We want to see how the second quantity changes when the first is modified. For example:     In order to visualize these things, we will need a two-dimensional image, where one direction corresponds to each quantity.          Plot and label the points , , , , , and .       Solutions of Equations  Now, what do we want to graph? Well, we're going to want to get graphs for mathematical equations (and later, functions). First, we need to look at solutions to these equations. If I have an equation in two variables and , then a solution is    Determine whether each ordered pair is a solution of the equation .                      Graphs of Equations  What is a graph of an equation?  Graphs of equations that look like are straight lines. Since it only takes two points to determine a straight line, we can draw these graphs by finding any two points. Two convenient points we can often use are the intercepts .    The intercepts of a graph are the places where the graph crosses the axes.            Graph .      Another way to graph equations of lines is to solve for first, then plug points into . In the normal way we will set up these equations in the future, we'll be looking to analyze how changes when we plug in different values of , so solving for is usually the way to go.    Graph .      When we solve out for like this, we are indicating that the value of depends on , so that is the dependent variable and is the independent variable .    Graph .    Proceed to Worksheet 01 on Graphing Equations.    "
+  "body": " Week 1   This is an outline of the topics we covered in the first week of class.     Monday 8\/31     Introduction to the class    Introduction to graphing solutions of equations.       Introduction to the Course  Welcome to College Algebra!  What are the goals of this course?       Introduction to the People  Who am I?  Who are all of you? Take about 3 minutes to introduce yourself to the people around you. These are going to be your companions on this semester-long journey that we have together. When we come back together, I'll want everyone to introduce themselves with their name and intended major\/career path.      Syllabus and Canvas Overview  First off, the syllabus. The entire syllabus is available on our Canvas site.     Basic info    Qualifying    eBook (more on that within Canvas later)    Attendance    Student Hours    Homework    Quizzes and Tests    Grade Policy and Final Exam Requirements    AI and Calculators     For the Canvas site:   Home page    Modules    Agenda page with notes    Pearson homework and eBook         Graphing  We're going to start this course by talking about graphing. There are many different types of graph, but the one we want to talk about in this class is the kind the allows us to compare two quantities. We want to see how the second quantity changes when the first is modified. For example:     In order to visualize these things, we will need a two-dimensional image, where one direction corresponds to each quantity.          Plot and label the points , , , , , and .       Solutions of Equations  Now, what do we want to graph? Well, we're going to want to get graphs for mathematical equations (and later, functions). First, we need to look at solutions to these equations. If I have an equation in two variables and , then a solution is    Determine whether each ordered pair is a solution of the equation .                      Graphs of Equations  What is a graph of an equation?  Graphs of equations that look like are straight lines. Since it only takes two points to determine a straight line, we can draw these graphs by finding any two points. Two convenient points we can often use are the intercepts .    The intercepts of a graph are the places where the graph crosses the axes.            Graph .      Another way to graph equations of lines is to solve for first, then plug points into . In the normal way we will set up these equations in the future, we'll be looking to analyze how changes when we plug in different values of , so solving for is usually the way to go.    Graph .      When we solve out for like this, we are indicating that the value of depends on , so that is the dependent variable and is the independent variable .    Graph .    Proceed to Worksheet 01 on Graphing Equations.     Wednesday 9\/2     Understand the definition of functions and relations, and determine whether a relation is a function.    Determine the domain and range of a function from points, formula definition, or graphs.       Functions  Assume that a university does not use a flat-rate tuition system. At this university, tuition is $2000 per semester with an additional $400 charged per credit hour.   How much does it cost to go there and take 1 credit hour?    What about 5 credit hours?    What about 18 credit hours?     This is an example of a function .       A function is    Before we see some examples of functions, it will be worthwhile to define a related concept first.    A relation is    How do these definitions compare?      Determine if the following diagrams correspond to functions.        Determine if each of the following correspondences are functions.        Domain  Students at Wayne State.    Range  All AccessIDs.    Correspondence  The AccessID for that student.            Domain  Mayoral election years in Detroit.    Range  All Mayors of Detroit.    Correspondence  The person who was elected that year.            Domain  NBA Teams who have won the national title.    Range  Years    Correspondence  The year each team won.            Domain  Years.    Range  NBA Teams.    Correspondence  The team that won the championship that year.          Another way to view relations (and functions) is as ordered pairs. So, the ordered pair would mean that the element in the domain corresponds to in the range.    Are each of the following relations, given as sets of ordered pars, functions? Explain why or why not. For each, determine the domain and range.                            Notation for Functions  The main way you want to think about functions is as machines.  For example, if we have an equation , we have  We want to give the function a name so that we can refer to it. Let's call it , which is the most common name used for functions. Then, instead of writing , we can write        For the function , compute                                   Graphs of Functions  If we have a function, we can draw it's graph.    Draw the graph of each of the following functions.                              From the graph given below, determine and .     f(x)=1\/2*(-x^4 + 3*x^2 - 4)+8              The Vertical Line Test  We can also draw the graph of any relation; we just plot all of the ordered pairs of the correspondence. If we do this, how do we know if the graph is that of a function?   Vertical Line Test          For each of the graphs below, determine if they correspond to graphs of functions?        Finding Domains  Recall the definition of domain from earlier:    For the function , evaluate , , , . What is the domain of this function?    What do we need to be concerned about when trying to determine the domain of a function?       For each of the functions below, find their domain.                                          Visualizing Domain and Range  The main things to keep in mind for seeing domain and range on graphs are       For each of the graphs below, determine the domain and range of the function.            f(x)=4-3*x                     f(x)=sqrt(x+1)                     f(x)=1\/x                      f(x)=1\/(x-2)^2                      f(x)=sqrt(9-(x+1)^2)               "
 },
 {
   "id": "notes-week-01-3-2",
@@ -80,6 +80,141 @@ var ptx_lunr_docs = [
   "number": "6",
   "title": "",
   "body": "  Graph .   "
+},
+{
+  "id": "notes-week-01-4-2",
+  "level": "2",
+  "url": "notes-week-01.html#notes-week-01-4-2",
+  "type": "Objectives",
+  "number": "",
+  "title": "",
+  "body": "   Understand the definition of functions and relations, and determine whether a relation is a function.    Determine the domain and range of a function from points, formula definition, or graphs.    "
+},
+{
+  "id": "notes-week-01-4-3-1-3",
+  "level": "2",
+  "url": "notes-week-01.html#notes-week-01-4-3-1-3",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "function "
+},
+{
+  "id": "def-function",
+  "level": "2",
+  "url": "notes-week-01.html#def-function",
+  "type": "Definition",
+  "number": "7",
+  "title": "",
+  "body": "  A function is   "
+},
+{
+  "id": "def-relation",
+  "level": "2",
+  "url": "notes-week-01.html#def-relation",
+  "type": "Definition",
+  "number": "8",
+  "title": "",
+  "body": "  A relation is   "
+},
+{
+  "id": "notes-week-01-4-5-1",
+  "level": "2",
+  "url": "notes-week-01.html#notes-week-01-4-5-1",
+  "type": "Example",
+  "number": "9",
+  "title": "",
+  "body": "  Determine if the following diagrams correspond to functions.   "
+},
+{
+  "id": "notes-week-01-4-6-1",
+  "level": "2",
+  "url": "notes-week-01.html#notes-week-01-4-6-1",
+  "type": "Example",
+  "number": "10",
+  "title": "",
+  "body": "  Determine if each of the following correspondences are functions.        Domain  Students at Wayne State.    Range  All AccessIDs.    Correspondence  The AccessID for that student.            Domain  Mayoral election years in Detroit.    Range  All Mayors of Detroit.    Correspondence  The person who was elected that year.            Domain  NBA Teams who have won the national title.    Range  Years    Correspondence  The year each team won.            Domain  Years.    Range  NBA Teams.    Correspondence  The team that won the championship that year.       "
+},
+{
+  "id": "notes-week-01-4-7-2",
+  "level": "2",
+  "url": "notes-week-01.html#notes-week-01-4-7-2",
+  "type": "Example",
+  "number": "11",
+  "title": "",
+  "body": "  Are each of the following relations, given as sets of ordered pars, functions? Explain why or why not. For each, determine the domain and range.                        "
+},
+{
+  "id": "notes-week-01-4-9-1",
+  "level": "2",
+  "url": "notes-week-01.html#notes-week-01-4-9-1",
+  "type": "Example",
+  "number": "12",
+  "title": "",
+  "body": "  For the function , compute                               "
+},
+{
+  "id": "notes-week-01-4-10-1-3",
+  "level": "2",
+  "url": "notes-week-01.html#notes-week-01-4-10-1-3",
+  "type": "Example",
+  "number": "13",
+  "title": "",
+  "body": "  Draw the graph of each of the following functions.                        "
+},
+{
+  "id": "notes-week-01-4-11-1",
+  "level": "2",
+  "url": "notes-week-01.html#notes-week-01-4-11-1",
+  "type": "Example",
+  "number": "14",
+  "title": "",
+  "body": "  From the graph given below, determine and .     f(x)=1\/2*(-x^4 + 3*x^2 - 4)+8          "
+},
+{
+  "id": "def-VerticalLine",
+  "level": "2",
+  "url": "notes-week-01.html#def-VerticalLine",
+  "type": "Definition",
+  "number": "15",
+  "title": "Vertical Line Test.",
+  "body": " Vertical Line Test       "
+},
+{
+  "id": "notes-week-01-4-12-1-4",
+  "level": "2",
+  "url": "notes-week-01.html#notes-week-01-4-12-1-4",
+  "type": "Example",
+  "number": "16",
+  "title": "",
+  "body": "  For each of the graphs below, determine if they correspond to graphs of functions?   "
+},
+{
+  "id": "notes-week-01-4-13-1-3",
+  "level": "2",
+  "url": "notes-week-01.html#notes-week-01-4-13-1-3",
+  "type": "Example",
+  "number": "17",
+  "title": "",
+  "body": "  For the function , evaluate , , , . What is the domain of this function?   "
+},
+{
+  "id": "notes-week-01-4-14-1",
+  "level": "2",
+  "url": "notes-week-01.html#notes-week-01-4-14-1",
+  "type": "Example",
+  "number": "18",
+  "title": "",
+  "body": "  For each of the functions below, find their domain.                                      "
+},
+{
+  "id": "notes-week-01-4-16-1",
+  "level": "2",
+  "url": "notes-week-01.html#notes-week-01-4-16-1",
+  "type": "Example",
+  "number": "19",
+  "title": "",
+  "body": "  For each of the graphs below, determine the domain and range of the function.            f(x)=4-3*x                     f(x)=sqrt(x+1)                     f(x)=1\/x                      f(x)=1\/(x-2)^2                      f(x)=sqrt(9-(x+1)^2)            "
 },
 {
   "id": "wksht-graphing-equations",
